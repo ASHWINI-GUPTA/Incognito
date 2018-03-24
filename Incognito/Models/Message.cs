b6c621e062915002e2a9b50e96ad938cacfd2433
Ashwini Gupta <Ashwinigupta4455@hotmail.com>
2018-03-24 10:37:37 +0530

    Creating Message Model and MessageCategory.

---
+++ Incognito/Models/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incognito.Models
+{
+    public class Message
+    {
+        public Message()
+        {
+            //MessageCategories = new List<MessageCategory>();
+        }
+
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Id of Currently LoggedIn User... If LoggedIn Else Null
+        /// </summary>
+        public string UserId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Storing IP Address of the User for Security Concerns.
+        /// </summary>
+        //public string IpAddress { get; set; }
+
+        public DateTime SentTime { get; set; }
+
+        //public List<MessageCategory> MessageCategories { get; set; }
+    }
+}
---
+++ Incognito/Models/MessageCategory.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Incognito.Models
+{
+    public class MessageCategory
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
