f1dc5320c5cf1cc0000d115399209b1535332ccc
Ashwini Gupta <Ashwinigupta4455@hotmail.com>
2018-04-03 12:31:39 +0530

    Making UI better.

--- Incognito/Controllers/UserController.cs
+++ Incognito/Controllers/UserController.cs
@@ -1,11 +1,15 @@
 ﻿using Incognito.Data;
 using Incognito.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Incognito.Controllers
-{
+{ 
+    [Authorize]
     public class UserController : Controller
     {
         private ApplicationUserDbContext _userContext;
@@ -22,10 +26,22 @@ namespace Incognito.Controllers
             _userManager = userManager;
         }
 
+        //Index Page
         public async Task<IActionResult> Index()
         {
-            return View();
-        } 
+            ApplicationUser user = await GetCurrentUserAsync();
+            var userId = _userManager.GetUserId(User);
+            ViewData["User"] = $"{user.FirstName} {user.LastName}";
 
+            var userMassages = await _messageContext.Messages
+                .Where(u => u.RecevierId == userId)
+                .ToListAsync();
+
+            return View(userMassages);
+        }
+
+        //Get current logged in user's identifier
+        private Task<ApplicationUser> GetCurrentUserAsync() => 
+            _userManager.GetUserAsync(HttpContext.User);
     }
 }
--- Incognito/Models/ApplicationUser.cs
+++ Incognito/Models/ApplicationUser.cs
@@ -10,8 +10,11 @@ namespace Incognito.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
